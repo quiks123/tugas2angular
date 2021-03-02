d18374c492ef931e5d595a3f1292c6f851a0d4f4
quiks123 <c14180028@john.petra.ac.id>
2021-03-02 19:08:54 +0700

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DetailComponent } from './detail/detail.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, FormsModule, RouterModule.forRoute(ROUTES)  ],
+  declarations: [ AppComponent, HelloComponent, DetailComponent, FavoriteComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
