a726005ca485b6bedd8b75dfe1532ef947783ded
quiks123 <c14180028@john.petra.ac.id>
2021-03-02 19:36:15 +0700

    1 file modified

--- src/app/globalvar.service.ts
+++ src/app/globalvar.service.ts
@@ -2,14 +2,33 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalvarService {
-  private Datajudul: String = "" 
+  private Datajudul: String = ""
+  private Dataisi: String = ""
+  private Datatanggal: String = ""
+
   constructor() { }
 
    public getjudul(){
-     return this.Datajudul
+     return this.Datajudul;
    }
 
    public setjudul(judul : String){
      this.Datajudul = judul;
+   }
+
+   public getisi(){
+     return this.Dataisi;
+   }
+
+   public setisi(isi : String){
+     this.Dataisi = isi;
+   }
+
+   public gettanggal(){
+     return this.Datatanggal;
+   }
+
+   public settanggal(tanggal : String){
+     this.Datatanggal = tanggal;
    }
 }
